d79fecbe65feff005fef2550b06469f12d4d0db6
isaki0317 <matsuoisaki.0317@gmail.com>
2020-10-04 06:23:40 +0000

    [resigned]

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '8ad604a322427803b89d16e1c2d7b9e19cadfc1c2a16c99c45bd92ea2bf6b01adae107c02306e0c487506e4481f77afcf9ae0c65688fda0dbd592fe5a5ecbfae'
